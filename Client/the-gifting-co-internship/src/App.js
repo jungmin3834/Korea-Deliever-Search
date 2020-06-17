eaccdd0d1cf9850f717638383e084ddccaab0ca4
jungmin3834 <52225582+jungmin3834@users.noreply.github.com>
2020-06-17 17:50:59 +0900

    프로토타입 코드 및 구조 -- 완성 데이터 불러오기 및 테이블에 내용 뿌려주는 부분 -- 추가로 진행할 예정 : 코드 구조화 및 최적화 , 정리 (에러 가능성 있는 부분 줄일 예정)

--- Client/the-gifting-co-internship/src/App.js
+++ Client/the-gifting-co-internship/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Tracking from './screen/TrackingSearchScreen';
 import './css/App.css';
+import Background from './logo.jpg'
 
 function App() {
   return (
     <div className="App">
-        Init Project
+      <div  style={{width: "100%" , height:"50%" }}> 
+        <img src={Background} ></img>
+      </div>
+ 
+      <div style={{backgroundColor:"#efefef", width: "1130px", height: "1000px" ,margin:"0 auto"}}>
+          <div style={{margin:"10% 0% 8% 0%" }}>
+          <Tracking></Tracking>
+          </div>
+      </div>
     </div>
   );
 }
--- Server/server.js
+++ Server/server.js
@@ -1,24 +1,35 @@
 var express = require("express");
 const fetch = require('node-fetch');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var cors = require('cors');
 var app = express();
-
 const apiKey = "5Mp0JfZdnAi9aYO3WuBS8g";
 
+app.use(cors());
+
 app.get("", (req, res) => {
+  console.log("dd");
   res.send("엥");
 });
 
-app.get("/url", (req, res) => {
-
+app.get("/api/trackingInfo", (req, res) => {
+console.log("오긴옴");
+  res.header("Access-Control-Allow-Origin", "*");
   var getData = 't_key='+ apiKey +'&t_code='+ req.query.t_code +'&t_invoice='+req.query.t_invoice;
   var url = 'http://info.sweettracker.co.kr/api/v1/trackingInfo?';
   console.log(url+getData);
   fetch(url + getData)
     .then(res => res.text())
-    .then(json => res.send(json));
+    .then(json => {
+
+      console.log(json);
+      res.send(json)
+    });
+});
+app.all('/*', function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
 });
 
-app.listen(3000, () => {
- console.log("Server running on port 3000");
+app.listen(7878, () => {
+ console.log("Server running on port 7878");
 });
