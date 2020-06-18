f65b4bbe6ae450142ee79f03eb9ca0d0598a59e0
jungmin3834 <52225582+jungmin3834@users.noreply.github.com>
2020-06-18 19:01:27 +0900

    update Node Server //  DataContainer.js 생성 -> {history 정보 저장 및 testcase 정보를 저장하고 있음}

--- Server/DataContainer.js
+++ Server/DataContainer.js
@@ -1,27 +1,19 @@
-const express = require("express");
-const bodyParser = require('body-parser');
-const controll = require('./commendControl');
-const cors = require('cors');
-const app = express();
+const historyList = [];
 
-app.use(cors());
-app.use(bodyParser());
+const historyController = {
 
-app.get("/api/getAllHistory",(req,res)=>{controll.getAllHistory(req,res);});
+  historySearch(json){
+    if(history.length > 5)
+      history.shift();
+    history.push(json);
+  },
 
-app.get("/test/case1", (req, res) => {  controll.testCase(req,res); });
+}
 
-app.get("/api/getTrackingInfo", (req, res) => {controll.getTrackingInfo(req,res);});
-
-app.post("/api/trackingInfo", (req, res) => {  controll.trackingInfo(req,res);});
+module.exports.history = historyController;
 
 
-app.listen(7878, () => {
- console.log("Server running on port 7878");
-});
-
-
-const testCase1 = '{"result":"Y","senderName":"주식*","receiverName":"손병*","itemName":"카멜","invoiceNo":"631693783814","receiverAddr":"경기도 안양*****","orderNumber":null,"adUrl":null,"estimate":"15∼17시","level":5,"complete":false,"recipient":"","ite'+
+module.exports.testCase1 = '{"result":"Y","senderName":"주식*","receiverName":"손병*","itemName":"카멜","invoiceNo":"631693783814","receiverAddr":"경기도 안양*****","orderNumber":null,"adUrl":null,"estimate":"15∼17시","level":5,"complete":false,"recipient":"","ite'+
 'mImage":null,"trackingDetails":[{"time":1592265739000,"timeString":"2020-06-16 09:02:19","code":null,"where":"서울금천남독산","kind":"집화처리","telno":"070-4202-3324","telno2":"","remark":null,"level":2,"manName":"","manPic":""},{"time"'+
 ':1592298129000,"timeString":"2020-06-16 18:02:09","code":null,"where":"금천","kind":"간선상차","telno":"02-1588-1255","telno2":"","remark":null,"level":3,"manName":"","manPic":""},{"time":1592345114000,"timeString":"2020-06-17 07:05:14",'+
 '"code":null,"where":"곤지암Hub","kind":"간선하차","telno":"","telno2":"","remark":null,"level":3,"manName":"","manPic":""},{"time":1592346123000,"timeString":"2020-06-17 07:22:03","code":null,"where":"곤지암Hub","kind":"간선하차","telno"'+
--- Server/commendControl.js
+++ Server/commendControl.js
@@ -1,7 +1,11 @@
 const apiKey = "5Mp0JfZdnAi9aYO3WuBS8g";
 const fetch = require('node-fetch');
-
+const history = require('./DataContainer').history;
+const testCase1 = require('./DataContainer').testCase1;
 const controll = {
+  getAllHistory(req,res){
+      res.send(history);
+  },
   testCase(req,res){
       res.send(testCase1);
   },
@@ -10,9 +14,11 @@ const controll = {
     var getData = 't_key='+ apiKey +'&t_code='+ req.body.code +'&t_invoice='+req.body.invoice;
     var url = 'http://info.sweettracker.co.kr/api/v1/trackingInfo?';
     fetch(url + getData)
-      .then(res => res.text())
+      .then(res => res.json())
       .then(json => {
-        console.log(json);
+        json.invoice = req.body.invoice;
+        json.code = req.body.code;
+        history.historyUpdate(json);
         res.send(json)
     });
   },
@@ -21,25 +27,14 @@ const controll = {
     var getData = 't_key='+ apiKey +'&t_code='+ req.body.code +'&t_invoice='+req.body.invoice;
     var url = 'http://info.sweettracker.co.kr/api/v1/trackingInfo?';
     fetch(url + getData)
-      .then(res => res.text())
+      .then(res => res.json())
       .then(json => {
-        console.log(json);
+        json.invoice = req.body.invoice;
+        json.code = req.body.code;
+        history.historyUpdate(json);
         res.send(json)
       });
   },
 }
 
 module.exports = controll;
-
-
-const testCase1 = '{"result":"Y","senderName":"주식*","receiverName":"손병*","itemName":"카멜","invoiceNo":"631693783814","receiverAddr":"경기도 안양*****","orderNumber":null,"adUrl":null,"estimate":"15∼17시","level":5,"complete":false,"recipient":"","ite'+
-'mImage":null,"trackingDetails":[{"time":1592265739000,"timeString":"2020-06-16 09:02:19","code":null,"where":"서울금천남독산","kind":"집화처리","telno":"070-4202-3324","telno2":"","remark":null,"level":2,"manName":"","manPic":""},{"time"'+
-':1592298129000,"timeString":"2020-06-16 18:02:09","code":null,"where":"금천","kind":"간선상차","telno":"02-1588-1255","telno2":"","remark":null,"level":3,"manName":"","manPic":""},{"time":1592345114000,"timeString":"2020-06-17 07:05:14",'+
-'"code":null,"where":"곤지암Hub","kind":"간선하차","telno":"","telno2":"","remark":null,"level":3,"manName":"","manPic":""},{"time":1592346123000,"timeString":"2020-06-17 07:22:03","code":null,"where":"곤지암Hub","kind":"간선하차","telno"'+
-':"","telno2":"","remark":null,"level":3,"manName":"","manPic":""},{"time":1592346692000,"timeString":"2020-06-17 07:31:32","code":null,"where":"곤지암Hub","kind":"행낭포장","telno":"","telno2":"","remark":null,"level":3,"manName":"","man'+
-'Pic":""},{"time":1592346763000,"timeString":"2020-06-17 07:32:43","code":null,"where":"곤지암Hub","kind":"간선상차","telno":"","telno2":"","remark":null,"level":3,"manName":"","manPic":""},{"time":1592364852000,"timeString":"2020-06-17 1'+
-'2:34:12","code":null,"where":"동안","kind":"간선하차","telno":"","telno2":"","remark":null,"level":3,"manName":"","manPic":""},{"time":1592368238000,"timeString":"2020-06-17 13:30:38","code":null,"where":"경기안양범계","kind":"배달출발'+
-'(배달예정시간:15∼17시)","telno":"010-3669-3846","telno2":"01046244502","remark":null,"level":5,"manName":"이항범","manPic":""}],"productInfo":null,"zipCode":null,"firstDetail":{"time":1592265739000,"timeString":"2020-06-16 09:02:19","'+
-'code":null,"where":"서울금천남독산","kind":"집화처리","telno":"070-4202-3324","telno2":"","remark":null,"level":2,"manName":"","manPic":""},"completeYN":"N","lastDetail":{"time":1592368238000,"timeString":"2020-06-17 13:30:38","code":nul'+
-'l,"where":"경기안양범계","kind":"배달출발(배달예정시간:15∼17시)","telno":"010-3669-3846","telno2":"01046244502","remark":null,"level":5,"manName":"이항범","manPic":""},"lastStateDetail":{"time":1592368238000,"timeString":"2020-06-17'+
-' 13:30:38","code":null,"where":"경기안양범계","kind":"배달출발(배달예정시간:15∼17시)","telno":"010-3669-3846","telno2":"01046244502","remark":null,"level":5,"manName":"이항범","manPic":""}}';
